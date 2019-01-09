a06a99b68649044139263faca3360d9f92bd7cc8
Eva Saintier <eva.saintier@my-digital-school.org>
2019-01-09 16:50:39 +0100

    fin de journee

---
+++ dev/presentation-mds.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Candidature My Digital School</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/style-mds.css"> 
+	</head>
+
+	<body>
+		<div class="page-presentation">
+			<div class="logo">
+				<img class="logo-mds" src="elements/logo-mds.png">
+			</div>
+			<h1 class="titre-mds">Bienvenue sur le site d'inscription en ligne de My Digital School !</h1>
+			<div class="intro-mds">
+				<p>Nous te proposons de remplir ton dossier de candidature en ligne, simplement et facilement.</p>
+				<p>Plus d'information sur My Digital School Angers est également disponible en cliquant sur le bouton ci-dessous.</p>
+			</div>
+			<div class="boutons">
+				
+			</div>
+		</div>
+		
+	</body>
+</html>
