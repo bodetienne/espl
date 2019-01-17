3c36392f0d675986ee3dfa89053c06155ff0b617
alexPouivet <alexpouivet@hotmail.fr>
2019-01-17 11:03:36 +0100

    commit alex 2

---
+++ dev/aide-mds.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Candidature My Digital School</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/style-mds.css" media="all" />
+		<link rel="stylesheet" href="css/style-mds-alex.css">
+		<link rel="stylesheet" href="css/style-mds-header-footer.css">
+		<link rel="icon" type="img" href="elements/favicon.ico.png">
+	</head>
+
+	<body>
+		<main class="form-main">
+			<div class="block-main">
+				<div class="page-aide">
+          <div class="menu-left">
+          </div>
+          <div class="reponse-right">
+          </div>
+				</div>
+			</div>
+
+			<?php include 'footer.php';?>
+		</main>
+	    <script type="text/javascript" src="js/jquery.min.js"></script>
+      	<script type="text/javascript" src="js/effects.js"></script>
+	</body>
+
+</html>
