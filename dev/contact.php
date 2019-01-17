bcaf43b7b4aec6c09ea7051e868e1d9f9971cb46
Emma Hervé <emma.herve@my-digital-school.org>
2019-01-17 15:57:54 +0100

    page contact

---
+++ dev/contact.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+		<link rel="stylesheet" href="css/style-mds-emma.css" />
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style-mds-header-footer.css">
+    <link rel="stylesheet" type="text/css" href="css/style-mds.css" media="all" />
+
+		<meta charset="utf-8">
+		<title>Contact</title>
+</head>
+
+<body class="contact">
+
+  <div class="block-left">
+    <h2>Contacte-nous !</h2>
+    <h3>Tu peux aussi nous joindre par téléphone au 02.41.73.20.30</h3>
+  </div>
+
+  <div class="block-right">
+    <div class="cadre-contact">
+      <form class="contact2-form validate-form"> <!-- contact2-form validate-form -->
+        <span class="contact2-form-title"><!-- contact2-form-title -->
+          Envoie-nous un message !
+        </span>
+
+        <div class="wrap-input2 validate-input" data-validate="Ton nom est nécessaire">
+          <input class="input2" type="text" name="name">
+          <span class="focus-input2" data-placeholder="Nom"></span>
+        </div>
+        <div class="wrap-input2 validate-input" data-validate="Ton prénom est nécessaire">
+          <input class="input2" type="text" name="Prénom">
+          <span class="focus-input2" data-placeholder="Prénom"></span>
+        </div>
+
+        <div class="wrap-input2 validate-input" data-validate = "Une adresse mail valide est nécessaire: abc@lif.fr">
+          <input class="input2" type="text" name="email">
+          <span class="focus-input2" data-placeholder="Email"></span>
+        </div>
+
+        <div class="wrap-input2 validate-input" data-validate = "Un message est nécessaire">
+          <textarea class="input2" name="message"></textarea>
+          <span class="focus-input2" data-placeholder="Message"></span>
+        </div>
+
+        <div class="container-contact2-form-btn">
+          <div class="wrap-contact2-form-btn">
+            <div class="contact2-form-bgbtn"></div>
+            <button class="contact2-form-btn">
+              Envoyer ton Message
+            </button>
+          </div>
+        </div>
+      </form>
+
+    </div>
+  </div>
+
+<?php include 'footer.php';?>
+
+</body>
+
+</html>
