0b76770fbb2753e6642d32f74fc713838ce631eb
Alex Pouivet <alexpouivet@hotmail.fr>
2019-02-07 11:56:13 +0100

    fin page aide

--- dev/js/aide.js
+++ dev/js/aide.js
@@ -6,6 +6,7 @@ $('.title').click(function(e) {
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
+
     } else {
         $this.parent().parent().find('.questions').removeClass('show');
         $this.parent().parent().find('.questions').slideUp(350);
