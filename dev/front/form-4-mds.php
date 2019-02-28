2a9f31eb9ad522bc1b61b87f0fc00cc25bbc461f
etienne <etienne.bodet@my-digital-school.org>
2019-02-28 15:07:01 +0100

    alors sale pute il t'arrive quoi ?

---
+++ dev/front/form-4-mds.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Candidature My Digital School</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/style-mds.css" media="all" />
+		<link rel="stylesheet" href="css/style-mds-eva.css"> 
+		<link rel="stylesheet" href="css/style-mds-alex.css"> 
+		<link rel="stylesheet" href="css/style-mds-structure.css"> 		
+		<link rel="stylesheet" href="css/style-mds-header-footer.css"> 
+		<link rel="icon" type="img" href="elements/favicon.ico.png">
+	</head>
+
+	<body>
+		<main class="form-main">
+			<div class="block-main">		
+					<div class="aside">
+						<?php include 'header.php'; ?>
+						<div class="fond-martin">
+							<div class="mur">
+								<div class="col-1"></div>
+								<div class="col-2">
+									<div class="blanc"></div>
+									<div class="bleu"></div>
+								</div>
+								<div class="col-3"></div>
+							</div>
+							<div class="trottoir">
+								<div class="ligne-1"></div>
+								<div class="ligne-2"></div>
+							</div>
+							<div class="sol"></div>
+						</div>
+					</div>
+					<div class="main formulaire-4">
+						<img class="progression-mds" src="elements/barre-progression-mds-2.png">
+						<h1 class="titre-mds">Tes garants <span class="etape-garant">2/2</span></h1>
+						<div class="form-4">
+							<div class="form-zone">
+								<div class="form-gauche">
+									<div class="prenom-garant-1">		
+										<label class="label label-prenom">Prénom<span class="required"> *</span></label>
+										<div class="champ-prenom">
+											<input class="textfield-input" type="text" id="prenom-garant">
+										</div>						
+									</div>
+									<div class="nom-garant">		
+										<label class="label label-nom">Nom<span class="required"> *</span></label>
+										<div class="champ-nom">
+											<input class="textfield-input" type="text" name="textfield" id="nom">
+										</div>						
+									</div>
+									<div class="mail-garant">		
+										<label class="label label-mail-garant">E-mail<span class="required"> *</span></label>
+										<div class="champs-mail-garant">
+											<input class="textfield-input" type="text" name="textfield"id="mail">
+										</div>						
+									</div>
+
+									<div class="tel-port-garant">		
+										<label class="label label-tel-port">Tel. portable<span class="required"> *</span></label>
+										<div class="champ-tel-port-garant">
+											<input class="tel-input" type="tel" minlength="10" maxlength="10" size="14" name="telfield"id="tel-garant">
+										</div>						
+									</div>
+									<div class="lien-parente">
+										<label class="label label-lien-parente">Lien de parenté<span class="required"> *</span></label>
+										<div class="champ-lien-parente">
+											<input class="textfield-input" type="text" name="textfield" id="lien"><a href="#" class="bouton-info"><img class="bouton" src="elements/info-button.png"></a>
+										</div>
+									</div>								
+								</div>
+								<div class="divider-milieu"></div>
+								<div class="form-droite">
+									<div class="meme-adresse">
+										<p class="text-adresse">Même adresse que précédemment ?</p>
+										<input class="checkbox-input" type="checkbox" id="button-adresse"><label for="button-adresse"></label>
+									</div>
+									<div class="adresse-garant">		
+										<label class="label label-adresse">Adresse <span class="required"> *</span></label>
+										<div class="champs-adresse">
+											<input class="textfield-input" type="text" name="textfield"id="adresse">
+										</div>						
+									</div>
+									<div class="CP-ville">
+										<div class="cp-garant">
+											<label class="label label-cp">Code postal<span class="required"> *</span></label>
+											<div class="champ-cp">
+												<input class="number-input" type="number" id="cp">
+											</div>
+										</div>
+										<div class="ville-garant">	
+											<label class="label label-ville">Ville<span class="required"> *</span></label>
+											<div class="champs-ville">
+												<input class="textfield-input" type="text" name="textfield" id="ville">
+											</div>		
+										</div>
+									</div>								
+									<div class="profession-garant">	
+										<label class="label label-profession">Profession</label>
+										<div class="champ-profession">
+											<input class="textfield-input" type="text" id="profession"><a href="#" class="bouton-info"><img class="bouton" src="elements/info-button.png"></a>
+										</div>			
+									</div>
+									<div class="entreprise">		
+										<label class="label label-entreprise">Entreprise</label>
+										<div class="champ-entreprise">
+											<input class="textfield-input" type="text" id="entreprise"><a href="#" class="bouton-info"><img class="bouton" src="elements/info-button.png"></a>
+										</div>						
+									</div>
+								</div>
+							</div>
+
+							<div class="container-button">
+								<a class="lien precedent" href="form-3-mds.php">
+									<button id="previous" class="btn-previous" type="button">Précédent</button>
+								</a>
+								<a class="lien sauvegarder" href="#">
+									<button id="save" class="btn-save" type="button">Sauvegarder</button>
+								</a>
+								<a class="lien suivant" href="formation-1-mds.php">
+									<button id="suivant" class="btn-next" type="button" name="button">Suivant</button>
+								</a>
+							</div>
+						</div>						
+					</div>
+				</div>
+							
+
+			<?php include 'footer.php';?>
+		</main>
+	    <script type="text/javascript" src="js/jquery.min.js"></script>
+      	<script type="text/javascript" src="js/effects.js"></script>
+	</body>
+
+</html>
---
+++ dev/front/form-test-page.php
@@ -0,0 +1,51 @@
+<html>
+   <head>
+       <meta charset="utf-8">
+       <title>Test des éléments de formulaire</title>
+       <link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
+       <link rel="stylesheet" type="text/css" href="css/style-mds.css" media="all" />
+
+    </head>
+    <body>
+      <main class="form-main">  <!-- "form-main" doit toujours rester, mettre le vrai nom à coté :
+                                      ex) class="form-main form-contact" !-->
+        <h1 class="nom-form">Présente-toi !</h1>
+        <?php include('functions/form-elements.php'); ?>
+        <?php echo textfield("", "Prénom", "no", "yes"); ?>
+        <?php echo textfield("", "Nom", "no", "yes"); ?>
+
+
+        <h1 class="nom-form">Choisis la formation de tes rêves !</h1>
+        <div class="container-lgn">
+          <h2>Bachelor Web et Digital</h2>
+          <div class="lgn lgn-1">
+            <?php echo createbtn("1", "Année 1", "#707070", "white", "#17E5F5"); ?>
+            <?php echo createbtn("2", "Année 2", "#707070", "white", "#50A7FF"); ?>
+            <?php echo createbtn("3", "Année 3", "#707070", "white", "#F74D9D"); ?>
+          </div>
+          <h2>MBA</h2>
+          <div class="lgn lgn-2">
+            <?php echo createbtn("4", "Année 1", "#707070", "white", "#9B9B9B"); ?>
+            <?php echo createbtn("5", "Année 2", "#707070", "white", "#343434"); ?>
+          </div>
+        </div>
+
+        <h1 class="nom-form">Comment as-tu connu l'école ?</h1>
+        <div class="form-checklist">
+          <?php echo radiobutton("Bouche à oreille / relation / étudiant") ?>
+          <?php echo radiobutton("Internet (Précisez : moteur de recherche ...)") ?>
+          <?php echo radiobutton("Salon, lequel") ?>
+        </div>
+
+        <div class="nav-form">
+          <a href="#" class="btn-form btn-precedent">Précédent</a>
+          <a href="#" class="btn-form btn-enregistrer">Enregistrer</a>
+          <a href="#" class="btn-form btn-suivant">Suivant</a>
+        </div>
+
+
+      </main>
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+      <script type="text/javascript" src="js/effects.js"></script>
+    </body>
+</html>
